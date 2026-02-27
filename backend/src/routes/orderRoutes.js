c5d84dd3c71a72a2447eaf55557d97144abf9486
ahmedeali <ahmed.e_ali@outlook.com>
2026-02-27 13:34:04 +0400

    GET /api/orders/my end point added to Swagger

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -4,6 +4,18 @@ const router = express.Router();
 const { createOrder, getMyOrders, updateOrderStatus } = require('../controllers/orderController');
 const { protect, admin } = require('../middleware/authMiddleware');
 
+/**
+ * @swagger
+ * /api/orders/my:
+ *   get:
+ *     summary: Get logged in user's orders
+ *     tags: [Orders]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of user orders
+ */
 router.post('/', protect, createOrder);
 router.get('/my', protect, getMyOrders);
 router.put('/:id/status', protect, admin, updateOrderStatus);
